7f27cb32759d48c8c485ead1a4519058aa75940c
tu4mo <tu4mo@users.noreply.github.com>
2016-07-05 22:34:32 +0300

    Add user authentication

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 Create `.env` file in the root with the following variables:
 
 ```
+JWT_SECRET=<secret>
 MONGODB_URI=mongodb://<user>:<password>@<host>/<database>
 ```
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 // Require dependencies
 const bcrypt = require('bcrypt')
 const express = require('express')
+const jwt = require('jsonwebtoken');
 
 // Require models
 const User = require('../models/User')
@@ -8,14 +9,75 @@ const User = require('../models/User')
 // Set up Router
 const router = new express.Router()
 
-router.get('/login', (req, res) => {
-  //
+function isAuthenticated(req, res, next) {
+  const auth = req.headers['authorization']
+
+  if (!auth) {
+    return res.sendStatus(403)
+  }
+
+  const parts = auth.split(' ')
+  const token = parts[1]
+
+  console.log(token)
+
+  // decode token
+  if (token) {
+    // verifies secret and checks exp
+    jwt.verify(token, process.env.JWT_SECRET, (err, jwtPayload) => {
+      if (err) {
+        return res.sendStatus(403)
+      } else {
+        req.jwtPayload = jwtPayload
+        next()
+      }
+    })
+  } else {
+    return res.sendStatus(403)
+  }
+}
+
+/**
+ * POST /authenticate
+ */
+router.post('/authenticate', (req, res) => {
+  User.findOne({
+    email: req.body.email
+  }, (err, user) => {
+    if (err) throw err
+
+    if (!user) {
+      res.json({ success: false, message: 'User not found.' })
+    } else {
+      const hashedPassword = bcrypt.hashSync(req.body.password, user.salt)
+
+      if (user.password != hashedPassword) {
+        res.json({ success: false, message: 'Wrong password.' })
+      } else {
+        var token = jwt.sign({ sub: user._id }, process.env.JWT_SECRET, {
+          expiresIn: '1 days'
+        })
+
+        res.json({
+          success: true,
+          token: token
+        })
+      }
+    }
+  })
 })
 
-router.get('/logout', (req, res) => {
-  //
+router.get('/secrets', isAuthenticated, (req, res) => {
+  res.end('Secret')
 })
 
+router.get('/', (req, res) => {
+  res.end('Hello')
+})
+
+/**
+ * POST /users
+ */
 router.post('/users', (req, res) => {
   const { email, username, password } = req.body
 
