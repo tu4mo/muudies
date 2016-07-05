3dc7896050316ac027660828cdd561754dc91d90
tu4mo <tu4mo@users.noreply.github.com>
2016-07-05 13:34:34 +0300

    Set up initial API endpoints

---
+++ server/routes/api.js
@@ -0,0 +1,15 @@
+// Require dependencies
+const express = require('express')
+
+// Set up Router
+const router = new Router()
+
+router.get('/api/login', (req, res) => {
+  //
+})
+
+router.get('/api/logout', (req, res) => {
+  //
+})
+
+export default router
--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const app = express()
 // Set up port
 const port = process.env.PORT || 3000
 
+// Set up routes
+const api = require('./routes/api')
+app.use('/api', api)
+
 // Start the server
 app.listen(port, () => {
   console.log('Listening on port ' + port)
