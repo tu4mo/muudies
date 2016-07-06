92d36b68fd66d07a9f406d7d248b3a40a526ede1
tu4mo <tu4mo@users.noreply.github.com>
2016-07-06 12:13:47 +0300

    Add Babel support for class properties

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,13 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-          presets: ['es2015', 'react']
+          plugins: [
+            'transform-class-properties'
+          ],
+          presets: [
+            'es2015',
+            'react'
+          ]
         }
       }
     ]
