0a3617b60f82196097c86d42cd497abe1d3df72a
tu4mo <tu4mo@users.noreply.github.com>
2016-08-06 11:23:12 +0300

    Make Mood fields required

--- server/models/Mood.js
+++ server/models/Mood.js
@@ -3,16 +3,19 @@ const mongoose = require('mongoose')
 const moodSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+    ref: 'User',
+    required: true
   },
   mood: {
     type: Number,
     min: 0,
-    max: 100
+    max: 100,
+    required: true
   },
   added: {
     type: Date,
-    default: Date.now
+    default: Date.now,
+    required: true
   }
 })
 
